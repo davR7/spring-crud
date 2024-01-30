2841fe30dc4990b5868028c5f655cd990de1f1fe
Davidson Ratis <davidsonmakarios@gmail.com>
2024-01-30 12:59:01 -0300

    added - readAllStudents method to the StudentResource class

--- src/main/java/com/davr7/springcrud/resources/StudentResource.java
+++ src/main/java/com/davr7/springcrud/resources/StudentResource.java
@@ -1,9 +1,14 @@
 package com.davr7.springcrud.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.davr7.springcrud.entities.Student;
 import com.davr7.springcrud.services.StudentService;
 
 @RestController
@@ -12,5 +17,10 @@ public class StudentResource {
 	
 	@Autowired
 	private StudentService service;
-
+	
+	@GetMapping
+	public ResponseEntity<List<Student>> readAllStudents(){
+		List<Student> students = service.readAllStudents();
+		return ResponseEntity.ok().body(students);
+	}
 }
