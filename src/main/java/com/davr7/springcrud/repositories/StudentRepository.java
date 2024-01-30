de165140afbf89e28f7a87c6b1a49e53906eb9e9
Davidson Ratis <davidsonmakarios@gmail.com>
2024-01-30 12:25:29 -0300

    created - StudentRepository interface

---
+++ src/main/java/com/davr7/springcrud/repositories/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.davr7.springcrud.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.davr7.springcrud.entities.Student;
+
+public interface StudentRepository extends JpaRepository<Student, Long>{
+
+}
