9af06b3c0a5b2e0131abaf3103bffda558487317
Davidson Ratis <davidsonmakarios@gmail.com>
2024-01-30 12:44:48 -0300

    added - readAllStudents method to the StudentService class

--- src/main/java/com/davr7/springcrud/services/StudentService.java
+++ src/main/java/com/davr7/springcrud/services/StudentService.java
@@ -1,12 +1,20 @@
 package com.davr7.springcrud.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.davr7.springcrud.entities.Student;
 import com.davr7.springcrud.repositories.StudentRepository;
 
 @Service
 public class StudentService {
 	
+	@Autowired
 	private StudentRepository repository;
-
+	
+	public List<Student> readAllStudents(){
+		return repository.findAll();
+	}
 }
