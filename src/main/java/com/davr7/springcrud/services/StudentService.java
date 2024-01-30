092f27e7a6fdc4c191a2ac9dc62f792f3159e609
Davidson Ratis <davidsonmakarios@gmail.com>
2024-01-30 12:29:59 -0300

    created - StudentService class

---
+++ src/main/java/com/davr7/springcrud/services/StudentService.java
@@ -0,0 +1,12 @@
+package com.davr7.springcrud.services;
+
+import org.springframework.stereotype.Service;
+
+import com.davr7.springcrud.repositories.StudentRepository;
+
+@Service
+public class StudentService {
+	
+	private StudentRepository repository;
+
+}
