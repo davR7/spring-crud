8ca6703118f415282f3eb915458e637a3d563341
Davidson Ratis <davidsonmakarios@gmail.com>
2024-01-30 18:30:22 -0300

    added - readStudentById method to the StudentResource class

--- src/main/java/com/davr7/springcrud/resources/StudentResource.java
+++ src/main/java/com/davr7/springcrud/resources/StudentResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +23,11 @@ public class StudentResource {
 	public ResponseEntity<List<Student>> readAllStudents(){
 		List<Student> students = service.readAllStudents();
 		return ResponseEntity.ok().body(students);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Student> readStudentById(@PathVariable Long id){
+		Student student = service.readStudentById(id);
+		return ResponseEntity.ok().body(student);
 	}
 }
