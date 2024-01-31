76275dedc4fb6731666680172a4a10118d29787f
Davidson Ratis <davidsonmakarios@gmail.com>
2024-01-30 22:14:55 -0300

    added - Many-to-One association test

--- src/main/java/com/davr7/springcrud/config/TestConfig.java
+++ src/main/java/com/davr7/springcrud/config/TestConfig.java
@@ -29,14 +29,19 @@ public class TestConfig implements CommandLineRunner {
 		Student stu3 = new Student(null, "Ana Mendes", "maria@test.com", "995571002", "12345");
 		Student stu4 = new Student(null, "Carlos Dantas", "carlos@test.com", "995561821", "12345");
 		
-		Article art1 = new Article(null, "Conhecendo o Spring Boot.", "Spring Boot é uma ferramenta que facilita e agiliza o desenvolvimento de aplicativos da web e de microsserviços.", "....");
-		Article art2 = new Article(null, "Os Pilares do POO.", "Para entender POO, é fundamental compreender os quatro pilares que sustentam esse paradigma.", "....");
-		Article art3 = new Article(null, "O PHP morreu?", "PHP morreu? Suas melhorias contínuas, foco na criação de aplicações web dinâmicas e importância inegável no mundo da programação.", "....");
-		Article art4 = new Article(null, "O que é NodeJS?", "Node.js é um software de código aberto, multiplataforma, baseado no interpretador V8 do Google.", "....");
-		Article art5 = new Article(null, "Principais comandos do Git", "Explore os comandos fundamentais do Git que serão essenciais no início da sua jornada como DEV.", "....");
+		Article art1 = new Article(null, "Conhecendo o Spring Boot.", "Spring Boot é uma ferramenta que facilita e agiliza o desenvolvimento de aplicativos da web e de microsserviços.", "....", stu1);
+		Article art2 = new Article(null, "Os Pilares do POO.", "Para entender POO, é fundamental compreender os quatro pilares que sustentam esse paradigma.", "....", stu1);
+		Article art3 = new Article(null, "O PHP morreu?", "PHP morreu? Suas melhorias contínuas, foco na criação de aplicações web dinâmicas e importância inegável no mundo da programação.", "....", stu2);
+		Article art4 = new Article(null, "O que é NodeJS?", "Node.js é um software de código aberto, multiplataforma, baseado no interpretador V8 do Google.", "....", stu2);
+		Article art5 = new Article(null, "Principais comandos do Git", "Explore os comandos fundamentais do Git que serão essenciais no início da sua jornada como DEV.", "....", stu3);
+		 
+		stu1.getWrittenArticles().add(art1);
+		stu1.getWrittenArticles().add(art2);
+		stu2.getWrittenArticles().add(art3);
+		stu2.getWrittenArticles().add(art4);
+		stu3.getWrittenArticles().add(art5);
 		
 		stuRepository.saveAll(Arrays.asList(stu1, stu2, stu3, stu4));
 		artRepository.saveAll(Arrays.asList(art1, art2, art3, art4, art5));
 	}
-
 }
