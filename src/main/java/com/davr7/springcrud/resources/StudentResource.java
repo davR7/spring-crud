34e2eacbbe01062aca9010b0ceb6698dd428002a
Davidson Ratis <davidsonmakarios@gmail.com>
2024-01-31 13:57:13 -0300

    added - createStudent, updateStudent and removeStudent methods to the StudentResource class

--- src/main/java/com/davr7/springcrud/resources/StudentResource.java
+++ src/main/java/com/davr7/springcrud/resources/StudentResource.java
@@ -1,13 +1,19 @@
 package com.davr7.springcrud.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.davr7.springcrud.entities.Student;
 import com.davr7.springcrud.services.StudentService;
@@ -29,5 +35,30 @@ public class StudentResource {
 	public ResponseEntity<Student> readStudentById(@PathVariable Long id){
 		Student student = service.readStudentById(id);
 		return ResponseEntity.ok().body(student);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Student> createStudent(@RequestBody Student obj){
+		Student student = service.createStudent(obj);
+		
+		URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(student.getId())
+                .toUri();
+		
+		return ResponseEntity.created(location).body(student);
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Student> updateStudent(@PathVariable Long id, @RequestBody Student obj) {
+		Student student = service.updateStudent(id, obj);
+		return ResponseEntity.ok().body(student);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> removeStudent(@PathVariable Long id){
+		service.removeStudent(id);
+		return ResponseEntity.noContent().build();
 	}
 }
