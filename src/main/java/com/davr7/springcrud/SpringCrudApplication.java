ed1e012a35b6ee7dc5f842f7ba2b5abc8cc0109a
Davidson Ratis <davidsonmakarios@gmail.com>
2024-01-31 14:02:53 -0300

    added - @EnableJpaAuditing annotation

--- src/main/java/com/davr7/springcrud/SpringCrudApplication.java
+++ src/main/java/com/davr7/springcrud/SpringCrudApplication.java
@@ -2,8 +2,10 @@ package com.davr7.springcrud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SpringCrudApplication {
 
 	public static void main(String[] args) {
