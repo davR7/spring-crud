60be9e5770b95cef21bd29b7173a1fce2f244cb3
Davidson Ratis <davidsonmakarios@gmail.com>
2024-01-31 23:03:21 -0300

    updated - ResourceNotFoundException construct public

--- src/main/java/com/davr7/springcrud/services/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/davr7/springcrud/services/exceptions/ResourceNotFoundException.java
@@ -3,7 +3,7 @@ package com.davr7.springcrud.services.exceptions;
 public class ResourceNotFoundException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 	
-	ResourceNotFoundException(Long id){
+	public ResourceNotFoundException(Long id){
 		super("Resource not found with id: "+ id);
 	}
 }
