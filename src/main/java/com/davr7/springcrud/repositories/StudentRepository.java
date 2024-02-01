48bdadd5587b5d30ed07d4519fb4a01fa504437e
Davidson Ratis <davidsonmakarios@gmail.com>
2024-02-01 12:49:26 -0300

    created - existsByEmail method to the StudentRepository class

--- src/main/java/com/davr7/springcrud/repositories/StudentRepository.java
+++ src/main/java/com/davr7/springcrud/repositories/StudentRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.davr7.springcrud.entities.Student;
 
 public interface StudentRepository extends JpaRepository<Student, Long>{
-
+	boolean existsByEmail(String email);
 }
