58b6685d9f0b471a17e7cea1002e8356dcd6a689
Davidson Ratis <davidsonmakarios@gmail.com>
2024-01-31 22:44:56 -0300

    created - GlobalExceptionHandler class

---
+++ src/main/java/com/davr7/springcrud/resources/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,10 @@
+package com.davr7.springcrud.resources.exceptions;
+
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+	
+}
