5b27f920eaf19201a53111d6dd99240e9da5fc79
Davidson Ratis <davidsonmakarios@gmail.com>
2024-02-02 15:35:16 -0300

    ErrorResponse extends StandardError

--- src/main/java/com/davr7/springcrud/resources/exceptions/ErrorResponse.java
+++ src/main/java/com/davr7/springcrud/resources/exceptions/ErrorResponse.java
@@ -2,38 +2,19 @@ package com.davr7.springcrud.resources.exceptions;
 
 import java.time.Instant;
 
-public class ErrorResponse {
-	private Instant timestamp;
-	private Integer statusCode;
-	private String error;
+public class ErrorResponse extends StandardError {
 	private String message;
-	private String path;
-	
-	public ErrorResponse(Instant timestamp, Integer statusCode, String error, String message, String path) {
-		this.timestamp = timestamp;
-		this.statusCode = statusCode;
-		this.error = error;
+
+	public ErrorResponse() {
+		super();
+	}
+
+	public ErrorResponse(Instant timestamp, String path, Integer statusCode, String error, String message) {
+		super(timestamp, path, statusCode, error);
 		this.message = message;
-		this.path = path;
-	}
-
-	public Instant getTimestamp() {
-		return timestamp;
-	}
-
-	public Integer getStatusCode() {
-		return statusCode;
-	}
-
-	public String getError() {
-		return error;
 	}
 
 	public String getMessage() {
 		return message;
-	}
-
-	public String getPath() {
-		return path;
 	}
 }
